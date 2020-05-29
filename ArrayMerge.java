736c3869ca3156a2fcd6cea51000dbdefc114233
Tharani54 <66108434+Tharani54@users.noreply.github.com>
2020-05-29 20:25:52 +0530

    Update ArrayMerge.java

--- ArrayMerge.java
+++ ArrayMerge.java
@@ -1,22 +1,20 @@
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayMerge {
 	public static void main (String[] args)    
 	
 	{   
-	Integer[] arr1 = new Integer[] {2,3,4,5,7,5,8,9,5};   
-	Integer[] arr2 = new Integer[]{1,5,6}; 
-	Object[] arr3 = resultarray(arr1,arr2);
-	System.out.println("Merged array: "+ Arrays.toString(arr3));  
-	
-	}    
 		
-	public static <T> Object[] resultarray(T[] arr1, T[] arr2)   
-	{   
-	return Stream.of(arr1,arr2).flatMap(Stream::of).toArray();  		
+		List<Integer> arr1 = new ArrayList<Integer>(List.of(2,3,4,5,7,5,8,9,5));
+		List<Integer> arr2 = new ArrayList<Integer>(List.of(1,5,9));
+		arr1.addAll(arr2);
+	  
+		List<Integer> ResultArray =Stream.concat( arr1.stream().filter(i -> !i.equals(5)),  
+				arr1.stream().filter(i -> i.equals(5))).collect(Collectors.toList()) ;
+		System.out.println(ResultArray);
 	}
-		  
+ 
 }
-	
